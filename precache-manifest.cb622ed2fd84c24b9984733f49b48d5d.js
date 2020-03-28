5e3b8da390d1f5bd7bef8b6597feab0d29973ec1
Paweł-Ś <pswiatlon02@gmail.com>
2020-03-28 14:23:02 +0100

    Updates

---
+++ precache-manifest.cb622ed2fd84c24b9984733f49b48d5d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f330550840903ce57c160d0f4338b3",
+    "url": "/JvPortfolio/index.html"
+  },
+  {
+    "revision": "deaba40b4e2ee18a234b",
+    "url": "/JvPortfolio/static/css/main.2e47dab9.chunk.css"
+  },
+  {
+    "revision": "2d8bf39cc896b7d799ab",
+    "url": "/JvPortfolio/static/js/2.24a1e7bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JvPortfolio/static/js/2.24a1e7bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deaba40b4e2ee18a234b",
+    "url": "/JvPortfolio/static/js/main.28208055.chunk.js"
+  },
+  {
+    "revision": "77f06dff1480f618d5ea",
+    "url": "/JvPortfolio/static/js/runtime-main.e9fcd71a.js"
+  },
+  {
+    "revision": "de3e44217ff6a106e0aa257ab721a5fa",
+    "url": "/JvPortfolio/static/media/01.de3e4421.jpg"
+  },
+  {
+    "revision": "d703e870de635c6b6ba1d68f7e42f559",
+    "url": "/JvPortfolio/static/media/02.d703e870.jpg"
+  },
+  {
+    "revision": "0aa9a69cde45f93db9e21a0efa9ab6a0",
+    "url": "/JvPortfolio/static/media/03.0aa9a69c.jpg"
+  },
+  {
+    "revision": "06431dfd0ef387c2bd7f6f66069a36a0",
+    "url": "/JvPortfolio/static/media/04.06431dfd.jpg"
+  },
+  {
+    "revision": "6475e1122aed8e92a464d3cacbc19270",
+    "url": "/JvPortfolio/static/media/05.6475e112.jpg"
+  },
+  {
+    "revision": "762bc0c881d7d53b502f3c11581ec12d",
+    "url": "/JvPortfolio/static/media/06.762bc0c8.jpg"
+  },
+  {
+    "revision": "e6656de0eabbba4482821e728af6e31f",
+    "url": "/JvPortfolio/static/media/bg.e6656de0.svg"
+  },
+  {
+    "revision": "49448f46e51b403964dd07fa0285b7b0",
+    "url": "/JvPortfolio/static/media/note2.49448f46.jpg"
+  },
+  {
+    "revision": "2c58897a74b7316c9443acc494376ff5",
+    "url": "/JvPortfolio/static/media/pc.2c58897a.jpg"
+  },
+  {
+    "revision": "b3ad4b5cc751037ec3a098a14bdae2f1",
+    "url": "/JvPortfolio/static/media/work.b3ad4b5c.jpg"
+  }
+]);
